708a52bca60a771e11fd0d3b2196e41f25a9599f
Ngo Luan <ngoducluan2004@gmail.com>
2026-03-12 16:28:14 +0700

    init project

---
+++ include/common.h
@@ -0,0 +1,42 @@
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <LittleFS.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+#include <freertos/semphr.h>
+
+#include <WiFi.h>              
+#include <AsyncTCP.h>           
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+
+#include <PubSubClient.h>
+#include <ThingsBoard.h>
+
+#include <TensorFlowLite_ESP32.h>
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+/* Define --------------------------------------------------------------------*/
+
+
+
+
+
+
+/* Struct --------------------------------------------------------------------*/
+
+
+
+
+
+
+#endif /* __COMMON_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "common.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hệ thống đang khởi động..."); // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
