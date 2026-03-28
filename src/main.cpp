2fcd7e7e5f013523c33f466e98e695938195649b
vominhquan1401 <vmquan1401@gmail.com>
2026-03-28 22:57:31 +0700

    add tinyML

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
-#include "common.h"
+#include <Arduino.h>
+#include "app_data_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+
+// Khai báo Queue Handle
+QueueHandle_t xSensorQueue;
+
+// Khai báo các Prototype Task
+void sensor_task(void *pvParameters);
+void tiny_ml_task(void *pvParameters); // Task bạn đã gửi trước đó
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Hệ thống đang khởi động..."); // put your setup code here, to run once:
+  delay(1000);
+  Serial.println("--- Smart Home RTOS Initializing ---");
+
+  // 1. Khởi tạo Queue (Sức chứa 5 gói tin, loại bỏ biến toàn cục)
+  xSensorQueue = xQueueCreate(5, sizeof(SensorData_t));
+
+  if (xSensorQueue != NULL)
+  {
+    // 2. Tạo Task đọc/giả lập cảm biến (Độ ưu tiên 1)
+    xTaskCreate(sensor_task, "Sensor_Task", 3072, NULL, 1, NULL);
+
+    xTaskCreate(tiny_ml_task, "TinyML_Task", 8192, NULL, 2, NULL);
+
+    Serial.println("FreeRTOS Tasks Started.");
+  }
+  else
+  {
+    Serial.println("Failed to create Queue!");
+  }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // Để trống vì FreeRTOS đã quản lý luồng chạy [cite: 6]
 }
